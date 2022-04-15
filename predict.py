5f9fdc16bd824959892babe6a65f523677dcaf53
= <=>
2022-04-15 22:43:45 +0530

    adfffffffffff

--- predict.py
+++ predict.py
@@ -13,20 +13,25 @@ def rmse(y_true, y_pred):
 model = models.load_model("./tensorflow", custom_objects={"rmse": rmse, "Adam": adam_v2})
 
 #make empty numpy array, copy format of the other one and then add an extra row for input.
-y = pd.DataFrame(needdict)
+y = np.zeros((2, 181)) #FIXME
+y2 = pd.DataFrame(y)
+X_train = pd.read_csv('carsaledataset.csv')
+X_train = X_train.drop(X_train.index.to_list(), axis = 0)
+X_train = X_train.drop(['selling_price', 'min_cost_price', 'max_cost_price', 'sale_state'], axis = 1)
+X_train.add(y)
 
 transformer = make_column_transformer(
     (MinMaxScaler(), 
         ['engine', 'mileage', 'seats', 
          'vehicle_age', 'km_driven', 'max_power']),
     (OneHotEncoder(handle_unknown='ignore'), 
-        ['brand', 'model', 'fuel_type', 'sale_state'])
+        ['brand', 'model', 'fuel_type'])
 )
 
-transformer.fit(y)
+transformer.fit(X_train)
 
-y = transformer.transform(y)
+X_train = transformer.transform(X_train)
 
-predict = model.predict(y)
+predict = model.predict(X_train + y) 
 
 print(predict)
