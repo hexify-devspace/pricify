b7c011d45cb8f33dddcca9060d07a011fc3dce9e
SyedAhkam <smahkam57@gmail.com>
2022-04-16 18:50:49 +0530

    result endpoint

--- backend/main.py
+++ backend/main.py
@@ -16,3 +16,20 @@ async def get_categories(request):
 @app.get("/filters/<category_id>")
 async def get_filters(request, category_id):
     return json(FILTERS[category_id])
+
+@app.post("/get_result")
+async def generate_result(request):
+    data = request.json
+    engine = data["engine"]
+    mileage = data["mileage"]
+    seats = data["seats"]
+    vehicle_age = data["vehicle_age"]
+    km_driven = data["km_driven"]
+    max_power = data["max_power"]
+    brand = data["brand"]
+    model = data["model"]
+    fuel_type = data["fuel_type"]
+
+    res = gen_result()
+
+    return json(res)
