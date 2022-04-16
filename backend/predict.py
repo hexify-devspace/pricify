9c8e39202c1d44dfb45315bebcbd8035dd5f20b0
= <=>
2022-04-16 20:58:20 +0530

    final commit before round 3

--- backend/predict.py
+++ backend/predict.py
@@ -12,6 +12,8 @@ import os
 from pathlib import Path
 
 MODEL_PATH = os.path.join(Path(__file__).parent, "model")
+DATASET_PATH = os.path.join(Path(__file__).parent, "datasets", "carsale.csv")
+
 
 def rmse(y_true, y_pred):
     return K.sqrt(K.mean(K.square(y_pred - y_true)))
@@ -24,18 +26,23 @@ model = models.load_model(
     }
 )
 
-#make empty numpy array, copy format of the other one and then add an extra row for input.
-y = pd.DataFrame([]) # FIXME
 
-transformer = make_column_transformer((
-    MinMaxScaler(),
-    ['brand', 'model', 'fuel_type', 'sale_state']
-))
+#make empty numpy array, copy format of the other one and then add an extra row for input.
+y = pd.read_csv(DATASET_PATH)
+y = y.drop(['min_cost_price', 'max_cost_price', 'selling_price'] , axis=1)
+
+transformer = make_column_transformer(
+    (MinMaxScaler(), 
+        ['engine', 'mileage', 'seats', 
+         'vehicle_age', 'km_driven', 'max_power']),
+    (OneHotEncoder(handle_unknown='ignore'), 
+        ['brand', 'model', 'fuel_type', 'sale_state'])
+)
 
 transformer.fit(y)
 
 y = transformer.transform(y)
+y = y.toarray()[:, 0:180]
+prediction = model.predict(y)
 
-predict = model.predict(y)
-
-print(predict)
+print(np.expm1(prediction[:5]))
